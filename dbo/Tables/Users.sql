6b09558a976939cc49f685b24d1d53ab211288e8
Ibrahim Chehade <Eng.IbrahimChehade@hotmail.com>
2022-08-03 01:10:25 +0300

    Fixing Tables

---
+++ dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [UserName] VARCHAR(50) NOT NULL,
+    [Password] VARCHAR(50) NOT NULL,
+    [Email] VARCHAR(50) NOT NULL, 
+    [Age] INT NULL, 
+    [CreatedDate] DATETIME NOT NULL
+)
